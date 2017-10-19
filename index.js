7cce1530f3ed82405071d88f6ae27bd63eb46091
bredele <olivier.wietrich@gmail.com>
2017-10-19 10:28:56 -0600

    Create value from schema key different than object

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
  * @api public
  */
 
-module.exports = function (data, schema) {
+module.exports = function (data, schema = {}) {
   const result = Object.assign({}, data)
+  Object.keys(schema).map(key => {
+    if (typeof key !== 'object') result[key] = schema[key]
+  })
   return result
 }
